cdc0fbf8dcdc9bc2fb3337aa9f391ea40bdafdc2
Michael Cannucci <michaelcannucci@gmail.com>
2021-09-18 13:55:24 -0400

    allow button entry point

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,43 @@
 require('dotenv').config();
-import { Client, Collection, Intents } from 'discord.js';
-import commands, {CommandExecutor} from './commands';
+import { ButtonInteraction, Client, Collection, CommandInteraction, Intents } from 'discord.js';
+import commands, { Executors } from './commands';
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_VOICE_STATES] });
+
+const hasCommandConext = (context: any): context is {command: string} => {
+    return context.command !== undefined && typeof context.command === "string";
+}
 
 client.once('ready', () => {
     console.log('Ready!');
 });
 client.login(process.env.TOKEN);
 
-const collection = new Collection<string, CommandExecutor>();
+const collection = new Collection<string, Executors>();
 commands.forEach(command => {
-    collection.set(command.info.name, command.execute);
+    collection.set(command.info.name, command.executors);
 });
-// 
+
 client.on('interactionCreate', async interaction => {
-    if (!interaction.isCommand()) return;
-    const command = collection.get(interaction.commandName);
-    if (!command) return;
-    try {
-        await command(interaction);
-    } catch (error) {
-        console.error(error);
-        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+    // Probably an better way to do this
+    if (interaction.isCommand()) {
+        try {
+            const command = collection.get(interaction.commandName).command;
+            await command(interaction);
+        } catch (error) {
+            console.error(error);
+            await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+        }
+    } else if (interaction.isButton()) {
+        try { 
+            const context = JSON.parse(interaction.customId);
+            if(hasCommandConext(context)) {
+                const command = collection.get(context.command).button
+                await command(interaction);
+            }
+        } catch (error) { 
+            console.error(error);
+            await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+        }
     }
 });
