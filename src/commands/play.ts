8456a1507be72f260899098b2ae61972ae3457df
Michael Cannucci <michaelcannucci@gmail.com>
2021-09-18 13:56:34 -0400

    refactor audio playing to handle unexpected states

--- src/commands/play.ts
+++ src/commands/play.ts
@@ -7,17 +7,29 @@ import {
     AudioPlayerStatus,
     AudioResource,
     VoiceConnection,
+    AudioPlayer,
+    entersState,
 } from "@discordjs/voice";
 import {
+    ButtonInteraction,
+    Collection,
     CommandInteraction,
     Guild,
+    InteractionCollector,
     MessageActionRow,
     MessageButton,
+    MessageComponentInteraction,
+    Snowflake,
     StageChannel,
+    TextBasedChannels,
+    User,
     VoiceChannel
 } from "discord.js";
 import { SlashCommandBuilder } from "@discordjs/builders";
 import * as ytdl from "ytdl-core";
+import { channel } from "diagnostics_channel";
+
+const playerCollection = new Collection<Snowflake, AudioPlayer>();
 
 const info = new SlashCommandBuilder();
 info
@@ -28,55 +40,68 @@ info
             .setDescription("Youtube Link with the song")
             .setRequired(true));
 
-const execute = async (interaction: CommandInteraction) => {
+const handleCommand = async (interaction: CommandInteraction) => {
     if (!interaction.guild.available) return;
     const { guild, user } = interaction;
     const url = interaction.options.getString("input", true);
-    if(!/http(s)?:\/\/www.youtube.com\/.*/.test(url)) {
-        await interaction.reply({content: "Only youtube links are supported", ephemeral: true }) 
+    if (!/http(s)?:\/\/www.youtube.com\/.*/.test(url)) {
+        await interaction.reply({ content: "Only youtube links are supported", ephemeral: true })
         return;
     }
-    const player = createAudioPlayer();
-    const channel = (await guild.channels.fetch())
-        .filter((channel): channel is VoiceChannel | StageChannel => channel.isVoice())
-        .filter(channel => channel.members.has(user.id))
-        .first();
-    if (!channel) {
-        await interaction.reply({ content: "You are not connected to any voice channels!", ephemeral: true });
+    const voiceChannel = await getVoiceChannel(guild, user);
+    if (voiceChannel instanceof Error) {
+        await interaction.reply({ content: voiceChannel.message, ephemeral: true });
         return;
     }
-    if (!channel.joinable) {
-        await interaction.reply({ content: "I can't join your voice channel", ephemeral: true });
-        return;
-    }
-    const connection = getConnection(channel);
-    connection.subscribe(player);
-    connection.on(VoiceConnectionStatus.Ready, () => {
-        player.play(getSong(url));
-    })
-    const collector = interaction.channel.createMessageComponentCollector();
-    collector.on("collect", async pressed => {
-        switch(pressed.customId) {
-            case "play":
-                if(player.state.status === AudioPlayerStatus.Idle) {
-                    player.play(getSong(url))
-                } else {
-                    player.unpause();
-                }
-            break;
-            case "pause":
-                player.pause();
-            break;
-            case "stop":
-                player.stop();
-            break;
-        }
-        await pressed.deferUpdate()
-    })
+    const player = getPlayer(voiceChannel.guild);
+    playSong(player, voiceChannel, url);
     await interaction.reply({
         content: `Now Playing: ${url}`,
-        components: [getPlayerButtons()]
+        components: [getPlayerButtons(url)],
     });
+}
+
+const handleButton = async (interaction: ButtonInteraction) => {
+    const context = JSON.parse(interaction.customId);
+    const { guild, user } = interaction;
+    const voiceChannel = await getVoiceChannel(guild, user);
+    if (voiceChannel instanceof Error) {
+        await interaction.reply({ content: voiceChannel.message, ephemeral: true });
+        return;
+    }
+    if (isPlayButton(context)) {
+        const player = getPlayer(voiceChannel.guild);
+        playSong(player, voiceChannel, context.url);
+    } else if (isPauseButton(context)) {
+        const player = getPlayer(voiceChannel.guild);
+        player.pause();
+    } else if (isStopButton(context)) {
+        const player = getPlayer(voiceChannel.guild);
+        player.stop();
+    }
+    await interaction.deferUpdate();
+}
+
+const playSong = (player: AudioPlayer, voiceChannel: VoiceChannel | StageChannel, url: string) => {
+    if(player.state.status === AudioPlayerStatus.Playing) {
+        player.stop();   
+    }
+    const connection = getConnection(voiceChannel);
+    connection.subscribe(player);
+    if(connection.state.status === VoiceConnectionStatus.Ready) {
+        player.play(getSong(url));
+    } else {
+        connection.on(VoiceConnectionStatus.Ready, () => {
+            player.play(getSong(url));
+        })
+    }
+}
+
+const getPlayer = (guild: Guild): AudioPlayer => {
+    if (!playerCollection.has(guild.id)) {
+        playerCollection.set(guild.id, createAudioPlayer());
+    }
+    return playerCollection.get(guild.id);
 }
 
 const getSong = (url: string): AudioResource<null> => {
@@ -94,26 +119,79 @@ const getConnection = (channel: VoiceChannel | StageChannel): VoiceConnection =>
             guildId: channel.guildId,
             adapterCreator: channel.guild.voiceAdapterCreator
         })
+        connection.on(VoiceConnectionStatus.Disconnected, async (oldState, newState) => {
+            try {
+                await Promise.race([
+                    entersState(connection, VoiceConnectionStatus.Signalling, 5_000),
+                    entersState(connection, VoiceConnectionStatus.Connecting, 5_000),
+                ]);
+                // Seems to be reconnecting to a new channel - ignore disconnect
+            } catch (error) {
+                // Seems to be a real disconnect which SHOULDN'T be recovered from
+                connection.destroy();
+            }
+        });
     }
     return connection;
 }
 
-const getPlayerButtons = (): MessageActionRow => {
+const getVoiceChannel = async (guild: Guild, user: User): Promise<(VoiceChannel | StageChannel) | Error> => {
+    const voiceChannel = (await guild.channels.fetch())
+        .filter((channel): channel is VoiceChannel | StageChannel => channel.isVoice())
+        .filter(channel => channel.members.has(user.id))
+        .first();
+    if (!voiceChannel) {
+        return new Error("You are not connected to any voice channels!");
+    }
+    if (!voiceChannel.joinable) {
+        return new Error("I can't join your voice channel");
+    }
+    return voiceChannel;
+}
+
+const isPlayButton = (context: any): context is { id: "play", command: string, url: string } => {
+    return context.id === "play" && typeof context.command === "string" && typeof context.url === "string";
+}
+
+const isPauseButton = (context: any): context is { id: "pause", command: string } => {
+    return context.id === "pause" && typeof context.command === "string"
+}
+
+const isStopButton = (context: any): context is { id: "stop", command: string } => {
+    return context.id === "stop" && typeof context.command === "string"
+}
+
+const getPlayerButtons = (url: string): MessageActionRow => {
     return new MessageActionRow()
         .addComponents(
             new MessageButton()
-                .setCustomId('play')
+                .setCustomId(JSON.stringify({
+                    id: "play",
+                    command: info.name,
+                    url
+                }))
                 .setLabel('Play')
                 .setStyle('PRIMARY'),
             new MessageButton()
-                .setCustomId('pause')
+                .setCustomId(JSON.stringify({
+                    id: "pause",
+                    command: info.name,
+                }))
                 .setLabel('Pause')
                 .setStyle('PRIMARY'),
             new MessageButton()
-                .setCustomId('stop')
+                .setCustomId(JSON.stringify({
+                    id: "stop",
+                    command: info.name,
+                }))
                 .setLabel('Stop')
                 .setStyle('PRIMARY'),
         );
 }
 
-export default { info, execute };
+export default {
+    info, executors: {
+        command: handleCommand,
+        button: handleButton
+    }
+};
