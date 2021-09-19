ac22f48540b2ef418ae465734d796bb05ac8fea5
Michael Cannucci <michaelcannucci@gmail.com>
2021-09-19 19:38:41 -0400

    don't need to be in a voice channel for queue

--- src/commands/show-queue.ts
+++ src/commands/show-queue.ts
@@ -10,30 +10,24 @@ const info = new SlashCommandBuilder()
 const execute = async (interaction: CommandInteraction) => {
   if (!interaction?.guild?.available) return;
   const { guild, user } = interaction;
-  getVoiceChannel(guild, user)
-    .then(async (voiceChannel) => {
-      const player = getPlayer(voiceChannel.guild);
-      const nowPlayingName = player.currentSong?.name ?? "N/A";
-      const queueEmbed = new MessageEmbed()
-        .setColor("DARK_NAVY")
-        .setTitle("Audio Player Queue")
-        .addField(
-          `Now Playing: ${nowPlayingName}`,
-          player.currentSong?.link ?? "N/A"
-        )
-        .addFields(
-          player.songQueue.map((info, index) => {
-            return {
-              name: `(${index++}) info.name`,
-              value: info.link,
-            };
-          })
-        );
-      await interaction.reply({ embeds: [queueEmbed] });
-    })
-    .catch(async (exception) => {
-      await interaction.reply({ content: exception, ephemeral: true });
-    });
+  const player = getPlayer(interaction.guild);
+  const nowPlayingName = player.currentSong?.name ?? "N/A";
+  const queueEmbed = new MessageEmbed()
+    .setColor("DARK_NAVY")
+    .setTitle("Audio Player Queue")
+    .addField(
+      `Now Playing: ${nowPlayingName}`,
+      player.currentSong?.link ?? "N/A"
+    )
+    .addFields(
+      player.songQueue.map((info, index) => {
+        return {
+          name: `(${index++}) info.name`,
+          value: info.link,
+        };
+      })
+    );
+  await interaction.reply({ embeds: [queueEmbed] });
 };
 
 export default {
