b2f3cbb3ad2a8a98a7ea9e63da148021e296e6d9
Michael Cannucci <MichaelCannucci@gmail.com>
2022-04-15 20:48:21 -0400

    Fix song name in list (whoops) and better message when queue is empty

--- src/commands/show-queue.ts
+++ src/commands/show-queue.ts
@@ -9,21 +9,24 @@ const info = new SlashCommandBuilder()
 
 const execute = async (interaction: CommandInteraction) => {
   if (!interaction?.guild?.available) return;
-  const { guild, user } = interaction;
   const player = getPlayer(interaction.guild);
+  if(player.empty) {
+    await interaction.reply({ content: "No songs are in the queue" });
+    return;
+  }
   const nowPlayingName = player.currentSong?.name ?? "N/A";
   const queueEmbed = new MessageEmbed()
     .setColor("DARK_NAVY")
     .setTitle("Audio Player Queue")
     .addField(
       `Now Playing: ${nowPlayingName}`,
-      player.currentSong?.link ?? "N/A"
+      player.currentSong?.uri ?? "N/A"
     )
     .addFields(
       player.songQueue.map((info, index) => {
         return {
-          name: `(${index++}) info.name`,
-          value: info.link,
+          name: `(${++index}) ${info.name}`,
+          value: info.uri,
         };
       })
     );
